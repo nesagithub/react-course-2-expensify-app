bcae2ad2bfeaa85d24a9695a07db0fc13fdbccbe
Nenad Milic <nmilic@gmail.com>
2024-01-02 20:32:51 +0100

    before fb

---
+++ src/components/ExpensesSummary.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { connect } from "react-redux"
+import numeral from "numeral";
+import selectExpenses from '../selectors/expenses'
+import selectExpensesTotal from '../selectors/expenses-total'
+
+// export const ExpenseSummary = ({expensesCount, expensesTotal}) => {
+//   const expenseWord = expensesCount === 1 ? 'expense' : 'expenses'
+//   const formattedExpenseTotal = numeral(expensesTotal / 100).format('$0,0.00')
+//   return (
+//     <div>
+//       <h1>Viewing {expensesCount} {expenseWord} totaling {formattedExpenseTotal}</h1>
+//     </div>
+//   )
+// }
+
+// const mapStatetoProps = () => {
+//   const visibleExpenses = selectExpenses(state.expenses, state.filters)
+
+//   return {
+//     expensesCount: visibleExpenses.length,
+//     expensesTotal: selectExpensesTotal(visibleExpenses)
+//   }
+// }
+
+// export default connect(mapStatetoProps)(ExpenseSummary)
+
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+  const expenseWord = expenseCount === 1 ? 'expense' : 'expenses' ;
+  const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+  
+  return (
+    <div>
+      <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}</h1>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  const visibleExpenses = selectExpenses(state.expenses, state.filters);
+
+  return {
+    expenseCount: visibleExpenses.length,
+    expensesTotal: selectExpensesTotal(visibleExpenses)
+  };
+};
+
+export default connect(mapStateToProps)(ExpensesSummary);
